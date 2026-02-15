536fa6ad2263245529983ef095c921f9b5e53642
Chris Dias <chrisdias2311@gmail.com>
2026-02-16 00:35:05 +0530

    config: Hardcode GitHub and Jira authentication client IDs and URLs.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 export const API_URL = "https://api.useshiplog.com"
-export const GITHUB_CLIENT_ID = import.meta.env.VITE_GITHUB_CLIENT_ID;
-export const GITHUB_AUTH_URL = import.meta.env.VITE_GITHUB_AUTH_URL;
-export const JIRA_CLIENT_ID = import.meta.env.VITE_JIRA_CLIENT_ID;
-export const JIRA_AUTH_URL = import.meta.env.VITE_JIRA_AUTH_URL;
-export const JIRA_CALLBACK_URL = import.meta.env.VITE_JIRA_CALLBACK_URL;
+export const GITHUB_CLIENT_ID = "use-shiplog";
+export const GITHUB_AUTH_URL = "https://github.com/apps/use-shiplog/installations/new";
+export const JIRA_CLIENT_ID = "mz4FuS6QpLFTK2mzlmFR28zVIW0kQEni";
+export const JIRA_AUTH_URL = "https://auth.atlassian.com/authorize";
+export const JIRA_CALLBACK_URL = "https://api.useshiplog.com/v1/jira/install/callback";
