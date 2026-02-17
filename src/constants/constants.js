c38bedaa427eb2fcf2cf10a37981864d43e384ac
Chris Dias <chrisdias2311@gmail.com>
2026-02-17 14:40:17 +0530

    chore: Update API endpoints, client IDs, and callback URLs to production values.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
-export const API_URL = "http://localhost:5001"
-export const GITHUB_CLIENT_ID = "useshiplog";
-export const GITHUB_AUTH_URL = "https://github.com/apps/useshiplog/installations/new";
-export const JIRA_CLIENT_ID = "pHAPkSqsqFXy8hCnPM4j6xkbIZOiVsse";
+export const API_URL = "https://api.useshiplog.com"
+export const GITHUB_CLIENT_ID = "use-shiplog";
+export const GITHUB_AUTH_URL = "https://github.com/apps/use-shiplog/installations/new";
+export const JIRA_CLIENT_ID = "mz4FuS6QpLFTK2mzlmFR28zVIW0kQEni";
 export const JIRA_AUTH_URL = "https://auth.atlassian.com/authorize";
-export const JIRA_CALLBACK_URL = "https://nonconstructive-unquayed-carri.ngrok-free.dev/v1/jira/install/callback";
+export const JIRA_CALLBACK_URL = "https://api.useshiplog.com/v1/jira/install/callback";
